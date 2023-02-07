8853f915951986116fe98fc60756e40516f35fe8
jk6722 <jk6722@hanyang.ac.kr>
2023-02-07 15:40:02 +0900

    temp

--- src/main/resources/templates/signin/main.js
+++ src/main/resources/templates/signin/main.js
@@ -1,10 +1,21 @@
+import axios from 'axios'
+
 signinEl = document.querySelector('.signin_btn');
 signinEl.addEventListener('click', () => {
-  let id = document.querySelector('#id')
-  let name = document.querySelector('#name')
-  let pw = document.querySelector('#pass')
+  const id = document.querySelector('#id')
+  const name = document.querySelector('#name')
+  const pw = document.querySelector('#pass')
   if(id.value == "" || name.value == "" || pw.value == ""){
     alert("회원가입을 할 수 없습니다")
   }
   else location.ref = './login/login.html'
 })
+
+function singinPost() {
+  const id = document.querySelector('#id')
+  const name = document.querySelector('#name')
+  const pw = document.querySelector('#pass')
+
+  axios.post("/src/main/resources/")
+}
+
